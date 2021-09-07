8bb2f17bebcdca8aaaf64b92f3cb56d9d35afd35
Andrey Ivanov <andrey.ivanov@hexagon.com>
2021-09-07 12:33:47 +0700

    Implementation with console logging

--- main.cpp
+++ main.cpp
@@ -1,17 +1,81 @@
 #include <iostream>
+#include "Context.h"
 
-#ifdef BUILD_NUMBER
-constexpr int buildNumber = BUILD_NUMBER;
-#else
-constexpr int buildNumber = std::numeric_limits<int>::max();
-#endif
+class CommandInterpreter
+{
+public:
+	void interpret(const std::string & cmd, Context & context)
+	{
+		if (cmd == "{")
+		{
+			context.openScope();
+		}
+		else if (cmd == "}")
+		{
+			context.closeScope();
+		}
+		else
+		{
+			context.addCommand(Command{ cmd });
+		}
+	}
+};
 
 int main()
 {
-	std::cout << "build " << buildNumber << std::endl;
-	std::cout << "Hello, World!" << std::endl;
-	int dummyInput{ 0 };
-	std::cin >> dummyInput;
+	int staticBlockSize;
+	std::cin >> staticBlockSize;
+
+	{
+		auto reactors{ std::make_unique<ReactorAggregation>() };
+		reactors->addReactor(std::make_unique<ConsoleLogger>());
+
+		Context context{ static_cast<size_t>(staticBlockSize) };
+		context.setReactor(std::move(reactors));
+
+		CommandInterpreter interpreter;
+
+		std::vector<std::string> input{ "cmd1", "cmd2", "cmd3", "cmd4", "cmd5" };
+
+		for (const auto & line : input)
+		{
+			interpreter.interpret(line, context);
+		}
+	}
+
+	{
+		auto reactors{ std::make_unique<ReactorAggregation>() };
+		reactors->addReactor(std::make_unique<ConsoleLogger>());
+
+		Context context{ static_cast<size_t>(staticBlockSize) };
+		context.setReactor(std::move(reactors));
+
+		CommandInterpreter interpreter;
+
+		std::vector<std::string> input{
+			"cmd1", "cmd2",
+			"{",
+				"cmd3", "cmd4",
+			"}",
+			"{",
+				"cmd5", "cmd6",
+				"{",
+					"cmd7", "cmd8",
+				"}",
+				"cmd9",
+			"}",
+			"{",
+				"cmd10", "cmd11"
+		};
+
+		for (const auto & line : input)
+		{
+			interpreter.interpret(line, context);
+		}
+	}
+
+	std::cout << "Hello World!\n";
+	std::cin >> staticBlockSize;
 
 	return 0;
 }
